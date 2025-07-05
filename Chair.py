92d637b91801cd22ae86bd554d5441de962c626f
celinebells <celinebells07@gmail.com>
2025-07-05 20:04:22 +0530

    Initial commit

---
+++ Chair.py
@@ -0,0 +1,16 @@
+# a= ("Chicken","Milk","Chocolate")#tuple
+# b= {"Chicken","Milk","Chocolate"}#set
+# c= ["Chicken","Milk","Chocolate"]#list
+# d= { "Name":"Parnika Biswas", "Age":20}
+# print(d)
+# print(d.keys())
+# print(d.values())
+# print(d["Name"])
+# print(d["Age"])
+#
+#
+# print(type(a))
+# print(a)
+# a = 5
+# print(a)
+
